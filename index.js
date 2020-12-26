22a7440c2e7274023b87152a4c2bd809363a7914
Aaron Gonzalez <aarong.1503@gmail.com>
2020-12-25 21:48:57 -0500

    Mobile view: ready

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const images = [
 ];
 
 const presentations = [
-  `I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.`,
-  `If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. `,
+  `"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."`,
+  `"If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer." `,
 ];
 
 const names = [`Tanya Sinclair `, `John Tarkpor`];
@@ -22,6 +22,8 @@ let $presentationDescription = document.getElementById(
   "presentation--description"
 );
 let $img = document.getElementById("img");
+let $leftSelector = document.getElementById("left-selector");
+let $rightSelector = document.getElementById("right-selector");
 
 // ##-----------------##
 //         Inners
@@ -34,20 +36,42 @@ $presentationName.innerText = names[i];
 $presentationDescription.innerText = descriptions[i];
 $img.src = images[i];
 
+if (i === 0) {
+  $leftSelector.style.opacity = ".5";
+} else {
+}
+
+function checkSelector() {
+  if (i === 0) {
+    $leftSelector.style.opacity = ".5";
+    $rightSelector.style.opacity = "1";
+  } else if (i === 1) {
+    $rightSelector.style.opacity = ".5";
+    $leftSelector.style.opacity = "1";
+  } else {
+  }
+}
+
 function sum() {
-  i += 1;
-  $presentation.innerText = presentations[i];
-  $presentationName.innerText = names[i];
-  $presentationDescription.innerText = descriptions[i];
-  $img.src = images[i];
-  console.log(i);
+  if (i === 1) {
+  } else {
+    i += 1;
+    $presentation.innerText = presentations[i];
+    $presentationName.innerText = names[i];
+    $presentationDescription.innerText = descriptions[i];
+    $img.src = images[i];
+  }
+  checkSelector();
 }
 
 function minus() {
-  i -= 1;
-  $presentation.innerText = presentations[i];
-  $presentationName.innerText = names[i];
-  $presentationDescription.innerText = descriptions[i];
-  $img.src = images[i];
-  console.log(i);
+  if (i === 0) {
+  } else {
+    i -= 1;
+    $presentation.innerText = presentations[i];
+    $presentationName.innerText = names[i];
+    $presentationDescription.innerText = descriptions[i];
+    $img.src = images[i];
+  }
+  checkSelector();
 }
